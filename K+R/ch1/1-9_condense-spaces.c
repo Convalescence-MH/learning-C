f7fe800ca3a4a22290e5abcfb959373b1060d5a3
Connor Harris <amorack.harrisc@gmail.com>
2026-06-19 18:17:48 -0400

    Add exercise 1-9

---
+++ K+R/ch1/1-9_condense-spaces.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* Copy input to output, replace multiple blanks with a single one */
+int main() {
+    char c;
+    int nb = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            nb++;
+        }
+
+        else if (nb > 0) {
+            printf(" %c", c);
+            nb = 0;
+        }
+
+        else {
+            printf("%c", c);
+        }
+    }
+
+    return 0;
+}
